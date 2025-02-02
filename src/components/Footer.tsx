b88b630ee763a9f908372ada6d9ce4fc5d3f071b
aitrends.now <aitrends.now@gmail.com>
2025-02-02 19:22:26 +0800

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} aitrends.now. All rights reserved. For
+        the love of Tech.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
