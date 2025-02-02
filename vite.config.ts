c62d1559121ea35022c4745c67c015261e67f8fe
aitrends.now <aitrends.now@gmail.com>
2025-02-02 19:46:15 +0800

    Your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://aitrendsnow.github.io/atrends.now/',
+  base: 'https://aitrendsnow.github.io/aitrends.now/',
 });
