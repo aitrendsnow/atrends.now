fb48b7af809bb875cf8383e458536cf510ab0077
aitrends.now <aitrends.now@gmail.com>
2025-02-08 19:22:57 +0800

    Updating Awesome

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <p>
+    <footer style={{ fontSize: "0.6rem" }}>
+      <p style={{ margin: 0 }}>
         &copy; {new Date().getFullYear()} aitrends.now. All rights reserved. For
         the love of Tech.
       </p>
