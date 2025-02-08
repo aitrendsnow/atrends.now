7263bccd61ba7bcd3f4e1cd4112adf9b152546c3
aitrends.now <aitrends.now@gmail.com>
2025-02-08 18:50:47 +0800

    Updating Awesome

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const detectWebView = () => {
+  const isWebView =
+    navigator.userAgent.includes("FBAN") ||
+    navigator.userAgent.includes("FBAV") ||
+    navigator.userAgent.includes("Instagram");
+
+  if (isWebView) {
+    document.documentElement.style.setProperty("--footer-font-size", "0.7rem");
+  }
+};
+
+const Main = () => {
+  useEffect(() => {
+    detectWebView();
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<Main />);
