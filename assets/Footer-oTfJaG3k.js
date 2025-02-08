9a5c91829f34a351288c388558e4927926cd9a09
aitrends.now <aitrends.now@gmail.com>
2025-02-08 23:36:18 +0800

    Updates

--- assets/Footer-oTfJaG3k.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./vendor-rcmh0ru9.js";const t=()=>e.jsx("footer",{children:e.jsxs("p",{children:["© ",new Date().getFullYear()," aitrends.now. All rights reserved."]})});export{t as default};
