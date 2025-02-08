329d022607681f13862d342ea55021daa79d9d0a
aitrends.now <aitrends.now@gmail.com>
2025-02-08 21:34:18 +0800

    Updates

---
+++ assets/Footer-CaZwI5hj.js
@@ -0,0 +1,1 @@
+import{j as e}from"./vendor-BWeLvPBk.js";const t=()=>e.jsx("footer",{children:e.jsxs("p",{children:["© ",new Date().getFullYear()," aitrends.now. All rights reserved."]})});export{t as default};
