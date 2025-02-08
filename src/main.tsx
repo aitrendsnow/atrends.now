49f5f0316458d856edea95dfbb3535257553ccda
aitrends.now <aitrends.now@gmail.com>
2025-02-08 19:19:13 +0800

    Updating Awesome

--- src/main.tsx
+++ src/main.tsx
@@ -4,25 +4,6 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-const detectWebView = (): boolean => {
-  const userAgent = navigator.userAgent || navigator.vendor;
-  const isStandalone =
-    (window.navigator as unknown as { standalone?: boolean }).standalone ||
-    false;
-
-  return /FBAN|FBAV|Instagram|Twitter|Threads/i.test(userAgent) || isStandalone;
-};
-
-const applyWebViewStyles = () => {
-  if (detectWebView()) {
-    document.documentElement.style.setProperty("--footer-padding", "15px");
-    document.documentElement.style.setProperty("--footer-font-size", "0.75rem");
-  }
-};
-
-// Apply styles before app renders
-applyWebViewStyles();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
