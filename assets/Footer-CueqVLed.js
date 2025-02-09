4ccc91d0b5185b713094c7b73e20585a07af6cf9
aitrends.now <aitrends.now@gmail.com>
2025-02-09 10:17:30 +0800

    Updates

---
+++ assets/Footer-CueqVLed.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BDM5EBwV.js";const t=()=>e.jsx("footer",{children:e.jsxs("p",{children:["© ",new Date().getFullYear()," aitrends.now. All rights reserved."]})});export{t as default};
