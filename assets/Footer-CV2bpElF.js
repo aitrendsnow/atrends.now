fbf73985b07f776e17d783bb7def88e880c69d64
aitrends.now <aitrends.now@gmail.com>
2025-02-09 10:04:16 +0800

    Updates

--- assets/Footer-CV2bpElF.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BofSmUQU.js";const t=()=>e.jsx("footer",{children:e.jsxs("p",{children:["© ",new Date().getFullYear()," aitrends.now. All rights reserved."]})});export{t as default};
