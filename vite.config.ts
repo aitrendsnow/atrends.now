9e9b21f3c506f76945c3d54783753fe8d3cf2510
aitrends.now <aitrends.now@gmail.com>
2025-02-16 22:55:48 +0800

    sending awesome

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { visualizer } from 'rollup-plugin-visualizer';
 import viteCompression from 'vite-plugin-compression';
-import createHtmlConfig from 'vite-plugin-html-config'; // Default import!
+import createHtmlConfig from 'vite-plugin-html-config';
 
 export default defineConfig({
   plugins: [
     react(),
-    ViteMinifyPlugin(), // This *should* handle HTML minification
+    ViteMinifyPlugin(),
     createHtmlConfig({
-      // minify: true,  // REMOVE THIS LINE
     }),
     visualizer({ open: false }),
     viteCompression(),
@@ -19,7 +18,28 @@ export default defineConfig({
     postcss: './postcss.config.cjs',
   },
   build: {
-    rollupOptions: {},
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const fileName = assetInfo.name || 'default-asset-name';
+          let extType = fileName.split('.').at(-1);
+
+          // Handle the case where extType might be undefined
+          if (!extType) {
+            extType = 'unknown'; // Or some other suitable default
+          }
+
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          } else if (/woff|woff2/.test(extType)) {
+            extType = 'fonts';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
     outDir: 'dist',
   },
   base: 'https://aitrendsnow.github.io/aitrends.now/',
